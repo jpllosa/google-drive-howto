f40b98173bf229f7aca76dd35b5477cbfe577138
jpllosa <jopeellosa@gmail.com>
2016-07-16 17:04:11 +0800

    refactored

--- src/main/java/example/SaveFile.java
+++ src/main/java/example/SaveFile.java
@@ -1,9 +1,7 @@
 package example;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,49 +25,42 @@ public class SaveFile {
 	// to run: java username appName
 	public static void main(String[] args) throws Exception {
 		// read and write scope
-		List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE_READONLY,  DriveScopes.DRIVE_FILE);
-		
+		List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE_READONLY, DriveScopes.DRIVE_FILE);
+
 		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-		
+
 		// credentials will be in the credentials directory
 		// once you have stored credential, no need to log in
 		FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(new java.io.File("credentials"));
-        
-        // Load client secrets.
-        GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(jsonFactory, 
-        		new InputStreamReader(new FileInputStream(new java.io.File("credentials/client_id.json"))));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, 
-                googleClientSecrets, SCOPES)
-                .setDataStoreFactory(fileDataStoreFactory)
-                .setAccessType("offline")
-                .build();
-        
-        // authorize(userId)
-        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(args[0]);
-        // Credentials saved to credentialsFolder
-		
-		Drive driveService = new Drive.Builder(httpTransport, jsonFactory, credential)
-                .setApplicationName(args[1])
-                .build();
-		
-		
+		// Load client secrets.
+		GoogleClientSecrets googleClientSecrets = GoogleClientSecrets.load(jsonFactory,
+				new InputStreamReader(new FileInputStream(new java.io.File("credentials/client_id.json"))));
+
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory,
+				googleClientSecrets, SCOPES).setDataStoreFactory(fileDataStoreFactory).setAccessType("offline").build();
+
+		// authorize(userId)
+		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(args[0]);
+		// Credentials saved to credentialsFolder
+
+		Drive driveService = new Drive.Builder(httpTransport, jsonFactory, credential).setApplicationName(args[1])
+				.build();
+
 		File fileMetadata = new File();
-        fileMetadata.setTitle("howToSave.txt");
-        
-        File googleDriveFile = driveService.files().insert(fileMetadata, 
-        		new InputStreamContent("", new FileInputStream("howToSave.txt")))
-                .setFields("id")
-                .execute();
-        if (googleDriveFile == null) {
-            throw new Exception("Unable to write file");
-        } else {
-        	System.out.println("file saved...");
-        }
-		
-		
+		fileMetadata.setTitle("howToSave.txt");
+
+		File googleDriveFile = driveService.files()
+				.insert(fileMetadata, new InputStreamContent("", new FileInputStream("howToSave.txt"))).setFields("id")
+				.execute();
+		if (googleDriveFile == null) {
+			throw new Exception("Unable to write file");
+		} else {
+			System.out.println("file saved...");
+		}
+
 		httpTransport.shutdown();
 
 	}
